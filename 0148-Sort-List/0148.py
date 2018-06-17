c28aa2f3b60cb70fd2753b31e3372c9564a3e28b
luliyucoordinate <luliyucoordinate@outlook.com>
2018-06-17 09:03:44 +0800

    modify 148

--- 0148-Sort-List/0148.py
+++ 0148-Sort-List/0148.py
@@ -6,17 +6,60 @@ class ListNode:
 
 
 class Solution:
-    def insertionSortList(self, head):
+    def _merge(self, head1, head2):
+        if head1 == None:
+            return head2
+        if head2 == None:
+            return head1
+
+        h = ListNode(-1)
+
+        # ret = None
+        # if head1.val < head2.val:
+        #     ret = head1
+        #     head1 = head1.next
+        # else:
+        #     ret = head2
+        #     head2 = head2.next
+
+        tmp = h
+        while head1 != None and head2 != None:
+            if head1.val < head2.val:
+                tmp.next = head1
+                head1 = head1.next
+            else:
+                tmp.next = head2
+                head2 = head2.next
+            tmp = tmp.next
+        
+        tmp.next = head1 or head2
+
+        return h.next
+        
+
+    def sortList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-  
+        if head == None or head.next == None:
+            return head
+        
+        lat = pre = head
+        while lat.next != None and lat.next.next != None:
+            lat = lat.next.next
+            pre = pre.next
+
+        lat = pre
+        lat = lat.next
+        pre.next = None
+        return self._merge(self.sortList(head), self.sortList(lat))
+
 
 def createList():
     head = ListNode(0)
     cur = head
-    for i in range(1, 2):
+    for i in range(1, 6):
         cur.next = ListNode(i)
         cur = cur.next
     return head
@@ -32,5 +75,5 @@ def printList(head):
 if __name__ == "__main__":
     head = createList()
     printList(head)
-    res = Solution().reverseKGroup(head, 2)
+    res = Solution().sortList(head)
     printList(res)
