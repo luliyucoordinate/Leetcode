ee4e6563d5e9be17f845518be6d42d4bf0544b9e
coordinate <luliyucoordinate@outlook.com>
2018-09-09 19:18:39 +0800

    add 77

--- 0077-Combinations/0077.py
+++ 0077-Combinations/0077.py
@@ -1,14 +1,4 @@
-# import itertools
-# class Solution:
-#     def combine(self, n, k):
-#         """
-#         :type n: int
-#         :type k: int
-#         :rtype: List[List[int]]
-#         """
-#         nums = [i for i in range(1,n+1)]
-#         return list(itertools.combinations(nums,k))
-
+import itertools
 class Solution:
     def combine(self, n, k):
         """
@@ -16,24 +6,34 @@ class Solution:
         :type k: int
         :rtype: List[List[int]]
         """
-        result = list()
-        pool = [i for i in range(1, n + 1)]
-        if k > n:
-            return
-        indices = [i for i in range(k)]
-        result.append([pool[i] for i in indices])
-        print([i for i in reversed(range(k))])
-        while True:
-            for i in reversed(range(k)):
-                if indices[i] != i + n - k:
-                    break
-            else:
-                return result
+        nums = [i for i in range(1,n+1)]
+        return list(itertools.combinations(nums,k))
 
-            indices[i] += 1
-            for j in range(i+1, k):
-                indices[j] = indices[j-1] + 1
-            result.append([pool[i] for i in indices])    
+# class Solution:
+#     def combine(self, n, k):
+#         """
+#         :type n: int
+#         :type k: int
+#         :rtype: List[List[int]]
+#         """
+#         result = list()
+#         pool = [i for i in range(1, n + 1)]
+#         if k > n:
+#             return
+#         indices = [i for i in range(k)]
+#         result.append([pool[i] for i in indices])
+#         print([i for i in reversed(range(k))])
+#         while True:
+#             for i in reversed(range(k)):
+#                 if indices[i] != i + n - k:
+#                     break
+#             else:
+#                 return result
+
+#             indices[i] += 1
+#             for j in range(i+1, k):
+#                 indices[j] = indices[j-1] + 1
+#             result.append([pool[i] for i in indices])    
 
 if __name__ == '__main__':
     n, k = 4, 2
