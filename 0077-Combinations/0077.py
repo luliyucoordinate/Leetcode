119f1bd858da16da4b459280e82d35ee73922092
coordinate <luliyucoordinate@outlook.com>
2018-09-09 15:13:50 +0800

    add 77

---
+++ 0077-Combinations/0077.py
@@ -0,0 +1,41 @@
+# import itertools
+# class Solution:
+#     def combine(self, n, k):
+#         """
+#         :type n: int
+#         :type k: int
+#         :rtype: List[List[int]]
+#         """
+#         nums = [i for i in range(1,n+1)]
+#         return list(itertools.combinations(nums,k))
+
+class Solution:
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = list()
+        pool = [i for i in range(1, n + 1)]
+        if k > n:
+            return
+        indices = [i for i in range(k)]
+        result.append([pool[i] for i in indices])
+        print([i for i in reversed(range(k))])
+        while True:
+            for i in reversed(range(k)):
+                if indices[i] != i + n - k:
+                    break
+            else:
+                return result
+
+            indices[i] += 1
+            for j in range(i+1, k):
+                indices[j] = indices[j-1] + 1
+            result.append([pool[i] for i in indices])    
+
+if __name__ == '__main__':
+    n, k = 4, 2
+    print(Solution().combine(n, k))
+
