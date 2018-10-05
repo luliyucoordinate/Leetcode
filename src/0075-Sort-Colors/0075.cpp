ba0badc299c50c36f0d335e88dd7f36ad70c36f6
luliyucoordinate <luliyucoordinate@outlook.com>
2018-10-05 10:53:51 +0800

    refactor 75 cpp resolution

--- src/0075-Sort-Colors/0075.cpp
+++ src/0075-Sort-Colors/0075.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <cassert>
 using namespace std;
 
 static int x = []() {std::ios::sync_with_stdio(false); cin.tie(0); return 0; }();
-
 class Solution 
 {
 public:
     void sortColors(vector<int>& nums) 
     {
-        int zero = -1;
-        int two = nums.size();
-        for (int i = 0; i < two;)
+        int zeros = 0, ones = 0;
+        for (unsigned int i = 0;i < nums.size(); ++i)
         {
-            if (nums[i] == i) 
-                ++i;
-            else if (nums[i] == 2)            
-                swap(nums[i], nums[--two]);
-            else
-            {
-                assert(nums[i] == 0);
-                swap(nums[++zero], nums[i++]);
-            }
+            if (nums[i] == 0) ++zeros;
+            if (nums[i] == 1) ++ones; 
+        }
+        for (unsigned int i = 0; i < nums.size(); ++i)
+        {
+            if (i < zeros) nums[i] = 0;
+            if ((i >= zeros) and (i < (zeros + ones))) nums[i] = 1;
+            if (i >= (zeros + ones)) nums[i] = 2;
         }
     }
 };
