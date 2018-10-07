0f82bc51deadd7c959b969c46169bbdaa6d6fda2
luliyucoordinate <luliyucoordinate@outlook.com>
2018-10-07 13:56:25 +0800

    refactor 345 python solution

--- src/0345-Reverse-Vowels-of-a-String/0345.py
+++ src/0345-Reverse-Vowels-of-a-String/0345.py
@@ -4,24 +4,9 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        vowels = ['a', 'u', 'e', 'i', 'o', 'A', 'U', 'E', 'I', 'O']
-        l = 0
-        r = len(s) - 1
-        s = list(s)
-        while l < r:
-            if s[l] not in vowels:
-                l += 1
-                continue
-
-            if s[r] not in vowels:
-                r -= 1
-                continue
-
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
-        return ''.join(s)
-
+        vowels = set(['a', 'u', 'e', 'i', 'o', 'A', 'U', 'E', 'I', 'O'])
+        p = [i for i in s if i in vowels]
+        return ''.join([i if i not in vowels else p.pop() for i in s])
 
 if __name__ == "__main__":
     s = "hello"
