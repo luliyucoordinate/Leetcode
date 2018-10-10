b890cda04bcb3e0b1f4176bead358928b33b5cbd
luliyucoordinate <luliyucoordinate@outlook.com>
2018-10-10 09:28:41 +0800

    refactor 918 python solution

--- src/0918-Maximum-Sum-Circular-Subarray/0918.py
+++ src/0918-Maximum-Sum-Circular-Subarray/0918.py
@@ -1,33 +1,33 @@
 class Solution:
-    def maxSubarraySumCircular(self, a):
+    def maxSubarraySumCircular(self, A):
         """
         :type A: List[int]
         :rtype: int
-        """      
-        def kadane(nums): 
-            cur, result = 0, 0
-            for num in nums: 
-                cur += num 
-                if cur < 0: 
-                    cur = 0
-                if result < cur: 
-                    result = cur 
-            return result 
+        """
+        cur_max = 0
+        result_max = 0
+
+        for num in A:
+            cur_max += num
+            if cur_max <= 0:
+                cur_max = 0
+            if result_max < cur_max:
+                result_max = cur_max
         
-        max_a = max(a)
-        if max_a < 0:
-            return max_a
-
-        len_a = len(a) 
-        result1 = kadane(a) 
-        result2 = 0
-        for i in range(len_a): 
-            result2 += a[i] 
-            a[i] = -a[i] 
-
-        result2 += kadane(a) 
-
-        return max(result1, result2)
+        if result_max == 0:
+            return max(A)
+        
+        cur_min = 0
+        result_min = 0
+        
+        for num in A:
+            cur_min += num
+            if cur_min >= 0:
+                cur_min = 0
+            if result_min > cur_min:
+                result_min = cur_min
+       
+        return max(result_max, sum(A) - result_min)
         
 
 if __name__ == "__main__":
