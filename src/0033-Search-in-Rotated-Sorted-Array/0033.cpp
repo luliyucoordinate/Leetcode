7efa0c0c4a1635431ec5ab7a0126fa5f5b228bd2
coordinate <luliyucoordinate@outlook.com>
2018-10-20 15:34:17 +0800

    add 33

---
+++ src/0033-Search-in-Rotated-Sorted-Array/0033.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+static int x = [](){std::ios::sync_with_stdio(false);cin.tie(0);return 0;}();
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        if (nums.empty()) return -1;
+        int low = 0, high = nums.size() - 1;
+        while (low <= high)
+        {
+            int mid = (high - low)/2 + low;
+            if (target == nums[mid]) return mid;
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target and target <= nums[mid])
+                    high = mid - 1;
+                else low = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] <= target and target <= nums[high])
+                    low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+int main()
+{
+    vector<int> arr = {4,5,6,7,0,1,2};
+    int target = 0;
+    cout << Solution().search(arr, target);
+    return 0;
+}
+
---
+++ src/0033-Search-in-Rotated-Sorted-Array/0033.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
+
+if __name__ == "__main__":
+    nums = [4,5,6,7,0,1,2]
+    target = 0
+    print(Solution().search(nums, target))
