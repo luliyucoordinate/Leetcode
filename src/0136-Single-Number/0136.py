e1784269b84ca44b5f17ab030c9951f059011cce
coordinate <luliyucoordinate@outlook.com>
2018-11-09 10:27:37 +0800

    add 136 more pythonic solution

--- src/0136-Single-Number/0136.py
+++ src/0136-Single-Number/0136.py
@@ -1,14 +1,12 @@
+from functools import reduce
+from operator import xor
 class Solution:
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        result = 0
-        for num in nums:
-            result ^= num
-
-        return result
+        return reduce(xor, nums)
 
 if __name__ == "__main__":
     nums = [2,2,1]
