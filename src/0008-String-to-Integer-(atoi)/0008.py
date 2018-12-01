2a03fff7ac94b2d571ba9360bedbc8616e92216c
coordinate <luliyucoordinate@outlook.com>
2018-12-01 17:01:13 +0800

    fix 8 python solution error

--- src/0008-String-to-Integer-(atoi)/0008.py
+++ src/0008-String-to-Integer-(atoi)/0008.py
@@ -6,14 +6,14 @@ class Solution:
         """
         s = s.lstrip()
         length = len(s)
-        if length == 0 or str[0] not in '0123456789+-':
+        if length == 0 or s[0] not in '0123456789+-':
             return 0
         index = 1
         while index < length and s[index].isdigit():
             index += 1
         if index == 1 and s[0] in '+-':
             return 0
-        result = int(s[:i])
+        result = int(s[:index])
         if result < 0:
             return max(-2147483648, result)
         else:
