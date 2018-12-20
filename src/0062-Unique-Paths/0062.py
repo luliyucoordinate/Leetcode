d7258973be8b576a5e6d11738ecab2dad2ea137f
coordinate <luliyucoordinate@outlook.com>
2018-12-20 09:41:16 +0800

    add 62 python faster solution

--- src/0062-Unique-Paths/0062.py
+++ src/0062-Unique-Paths/0062.py
@@ -1,4 +1,3 @@
-import math 
 class Solution:
     def uniquePaths(self, m, n):
         """
@@ -6,9 +5,16 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        mole = math.factorial(m)
-        deno = math.factorial(n - r) * math.factorial(r)
-        return int(mole/deno)  
+        mem = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if i or j:
+                    mem[i][j] = mem[i-1][j] + mem[i][j-1]
+                else:
+                    mem[i][j] = 1
+                    
+        return mem[-1][-1]
 
 if __name__ == "__main__":
     m, n = 7, 3
