9fcb80319168854f01ac5fc0fad80e7421d4e2c7
coordinate <luliyucoordinate@outlook.com>
2018-12-27 10:52:35 +0800

    fix 109 python solution

--- src/0109-Convert-Sorted-List-to-Binary-Search-Tree/0109.py
+++ src/0109-Convert-Sorted-List-to-Binary-Search-Tree/0109.py
@@ -1,3 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
     def sortedListToBST(self, head):
         """
