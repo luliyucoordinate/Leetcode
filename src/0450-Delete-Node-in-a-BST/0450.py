de23b700a15235dd6bec4f7880dd31b92772291a
coordinate <luliyucoordinate@outlook.com>
2019-02-16 18:02:35 +0800

    update 450 python solution

--- src/0450-Delete-Node-in-a-BST/0450.py
+++ src/0450-Delete-Node-in-a-BST/0450.py
@@ -1,17 +1,4 @@
 class Solution:
-    def minimum(self, root):
-        if not root.left:
-            return root
-
-        return self.minimum(root.left)
-
-    def removeMin(self, root):
-        if not root.left:
-            return root.right
-        
-        root.left = self.removeMin(root.left)
-        return root
-
     def deleteNode(self, root, key):
         """
         :type root: TreeNode
@@ -30,12 +17,13 @@ class Solution:
         else:
             if not root.left:
                 return root.right
-
+            
             if not root.right:
                 return root.left
-
-            tmp = self.minimum(root.right)
-            tmp.right = self.removeMin(root.right)
-            tmp.left = root.left
-            return tmp
-      
+            
+            minNode = root.right
+            while minNode.left:
+                minNode = minNode.left
+            root.val = minNode.val
+            root.right = self.deleteNode(root.right, minNode.val)     
+            return root
