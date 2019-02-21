05947ca901f9db4dd23aa870939c3c26b9fb272d
coordinate <luliyucoordinate@outlook.com>
2019-02-21 09:59:09 +0800

    fix 10 cpp solution

--- src/0010-Regular-Expression-Matching/0010.cpp
+++ src/0010-Regular-Expression-Matching/0010.cpp
@@ -11,15 +11,15 @@ public:
         int m = s.length(), n = p.length(); 
         vector<vector<bool>> mem(m + 1, vector<bool> (n + 1, false));
         mem[0][0] = true;
-    	for (int j = 1; j <= n; j++) mem[0][j] = p[j-1] == '*' && mem[0][j-2];
+    	for (int j = 1; j <= n; j++) mem[0][j] = p[j-1] == '*' && j > 1 && mem[0][j-2];
         for (int i = 1; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
                 if (p[j - 1] == '*')
-                    mem[i][j] = mem[i][j - 2] || 
+                    mem[i][j] = (j > 1 && mem[i][j - 2]) || 
                     (i > 0 && (s[i - 1] == p[j - 2] || 
-                    p[j - 2] == '.') && mem[i - 1][j]);
+                    (j > 1 && p[j - 2] == '.')) && mem[i - 1][j]);
                 else mem[i][j] = i > 0 && 
                     mem[i - 1][j - 1] && 
                     (s[i - 1] == p[j - 1] || p[j - 1] == '.');
