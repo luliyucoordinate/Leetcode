fe4fbbe2857fc7c8e682d2742b488ca9e0e431f8
coordinate <luliyucoordinate@outlook.com>
2019-03-19 14:50:41 +0800

    fix 788 python error and update a trick

--- src/0788-Rotated-Digits/0788.py
+++ src/0788-Rotated-Digits/0788.py
@@ -1,10 +1,10 @@
 class Solution:
     def rotatedDigits(self, N: int) -> int:
-		s1 = set([0, 1, 8])
+        s1 = set([0, 1, 8])
         s2 = set([0, 1, 8, 2, 5, 6, 9])
         s = set()
         res = 0
-        N = map(int, str(N))
+        N = list(map(int, str(N+1)))
         for i, v in enumerate(N):
             for j in range(v):
                 if s.issubset(s2) and j in s2:
@@ -14,4 +14,4 @@ class Solution:
             if v not in s2:
                 return res
             s.add(v)
-        return res + (s.issubset(s2) and not s.issubset(s1))
+        return res
