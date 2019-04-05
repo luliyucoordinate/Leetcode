f19a62d0d22ab08070ca7658713f5b2e3e7f1a59
coordinate <luliyucoordinate@outlook.com>
2019-04-05 17:29:02 +0800

    fix 10 cpp solution vector<boo> refrence effective stl

--- src/0010-Regular-Expression-Matching/0010.cpp
+++ src/0010-Regular-Expression-Matching/0010.cpp
@@ -9,20 +9,18 @@ public:
     bool isMatch(string s, string p) 
     {
         int m = s.length(), n = p.length(); 
-        vector<vector<bool>> mem(m + 1, vector<bool> (n + 1, false));
+        deque<deque<bool>> mem(m + 1, deque<bool> (n + 1, false));
         mem[0][0] = true;
-    	for (int j = 1; j <= n; j++) mem[0][j] = p[j-1] == '*' && j > 1 && mem[0][j-2];
-        for (int i = 1; i <= m; i++)
+        for (int i = 0; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
                 if (p[j - 1] == '*')
                     mem[i][j] = (j > 1 && mem[i][j - 2]) || 
-                    (i > 0 && (s[i - 1] == p[j - 2] || 
-                    (j > 1 && p[j - 2] == '.')) && mem[i - 1][j]);
-                else mem[i][j] = i > 0 && 
-                    mem[i - 1][j - 1] && 
-                    (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                    ((i > 0 && j > 1 && s[i - 1] == p[j - 2]) || 
+                    (j > 1 && p[j - 2] == '.')) && i > 0 && mem[i - 1][j];
+                else mem[i][j] = (i > 0 && mem[i - 1][j - 1]) && 
+                    ((i > 0 && s[i - 1] == p[j - 1]) || p[j - 1] == '.');
             }
         }
         return mem[m][n];
