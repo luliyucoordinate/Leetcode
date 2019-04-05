67acb9e7d700c65a43af4ba7bfc4d71e01831c32
coordinate <luliyucoordinate@outlook.com>
2019-04-05 11:32:55 +0800

    update 4 cpp solution

--- src/0004-Median-of-Two-Sorted-Arrays/0004.cpp
+++ src/0004-Median-of-Two-Sorted-Arrays/0004.cpp
@@ -10,15 +10,12 @@ public:
     {
         unsigned int m = nums1.size(), n = nums2.size();
         unsigned int mid = (m + n + 1)/2;//maybe overflow
-        if (m > n)
-        {
-            auto tmp1 = nums1; nums1 = nums2; nums2 = tmp1;
-            unsigned int tmp2 = m; m = n; n = tmp2;
-        }
+        if (m > n) swap(nums1, nums2), swap(m, n);
+        
         unsigned int l = 0, r = m;
         while (l <= r)
         {
-            unsigned int i = (l + r) / 2;
+            unsigned int i = (r - l) / 2 + l;
             unsigned int j = mid - i;
             if (i < m and nums2[j-1] > nums1[i]) l = i + 1;
             else if (i > 0 and nums2[j] < nums1[i - 1]) r = i - 1;
@@ -36,8 +33,8 @@ public:
                 else min_right = min(nums1[i], nums2[j]);
                 return (min_right + max_left)/2.0;
             }
-
         }
+        return -1;
     }
 };
 int main()
