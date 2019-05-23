e3eb030bbf8d1f782ffed1679e3a5819e9d96696
coordinate <luliyucoordinate@outlook.com>
2019-05-23 15:43:40 +0800

    fix 188 cpp error

--- src/0188-Best-Time-to-Buy-and-Sell-Stock-IV/0188.cpp
+++ src/0188-Best-Time-to-Buy-and-Sell-Stock-IV/0188.cpp
@@ -9,17 +9,17 @@ public:
     int maxProfit(int k, vector<int>& prices) 
     {
         if (k >= prices.size() / 2) return greedy(prices);
-        vector<int> buys(k, -prices[0]), sells(k + 1, 0);
-        for (int i = 1; i < prices.size(); ++i)
+        vector<int> buys(k+1, -prices[0]), sells(k+1, 0);
+        for (int i = 0; i < prices.size(); ++i)
         {
-            for (int j = 0; j < k; ++j) 
+            for (int j = 1; j <= k; ++j) 
             {
                 buys[j] = max(buys[j], sells[j - 1] - prices[i]);
                 sells[j] = max(sells[j], buys[j] + prices[i]);  
             }
         }
 
-        return *max_element(sells.begin(), sells.end());
+        return sells.back();
     }
 private:
     int greedy(vector<int>& p) 
