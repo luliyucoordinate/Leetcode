71016f61dc2089eb426246e8a536388c9a4e9a42
coordinate <luliyucoordinate@outlook.com>
2019-07-03 08:33:49 +0800

    add 11 go

---
+++ src/0011-Container-With-Most-Water/0011.go
@@ -0,0 +1,26 @@
+func maxArea(height []int) int {
+    l, r, res := 0, len(height)-1, 0
+    for l < r {
+        res = max(res, (r - l)*min(height[l], height[r]))
+        if height[l] > height[r] {
+            r--
+        } else {
+            l++
+        }
+    }
+    return res
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
