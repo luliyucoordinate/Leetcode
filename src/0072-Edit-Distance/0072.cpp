2854cf2cd1ab3333d2e7c5f1d0f15b41394bcc4a
coordinate <luliyucoordinate@outlook.com>
2019-09-04 19:38:39 +0800

    fix 72 cpp solution

--- src/0072-Edit-Distance/0072.cpp
+++ src/0072-Edit-Distance/0072.cpp
@@ -8,7 +8,7 @@ class Solution
 public:
     int minDistance(string word1, string word2) 
     { 
-        int word1_len = word1.size(), word2_len = word2.size();
+        int m = word1.size(), n = word2.size();
         vector<vector<int>> mem(m + 1, vector<int>(n + 1, 0));
         for (int i = 1; i <= m; i++) mem[i][0] = i;
         for (int j = 1; j <= n; j++) mem[0][j] = j;  
