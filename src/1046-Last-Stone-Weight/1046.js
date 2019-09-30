62396a290ec5d8a2f773c121f672f4f8c8ef159a
coordinate <luliyucoordinate@outlook.com>
2019-09-30 13:58:19 +0800

    add 1046 js, go

---
+++ src/1046-Last-Stone-Weight/1046.js
@@ -0,0 +1,92 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+const top = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+
+class PriorityQueue {
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+
+    size() {
+        return this._heap.length;
+    }
+
+    isEmpty() {
+        return this.size() == 0;
+    }
+
+    peek() {
+        return this._heap[top];
+    }
+
+    push(...values) {
+        values.forEach(value => {
+            this._heap.push(value);
+            this._siftUp();
+        });
+        return this.size();
+    }
+
+    pop() {
+        const poppedValue = this.peek();
+        const bottom = this.size() - 1;
+        if (bottom > top) {
+            this._swap(top, bottom);
+        }
+        this._heap.pop();
+        this._siftDown();
+        return poppedValue;
+    }
+
+    replace(value) {
+        const replacedValue = this.peek();
+        this._heap[top] = value;
+        this._siftDown();
+        return replacedValue;
+    }
+
+    _greater(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+
+    _siftUp() {
+        let node = this.size() - 1;
+        while (node > top && this._greater(node, parent(node))) {
+            this._swap(node, parent(node));
+            node = parent(node);
+        }
+    }
+
+    _siftDown() {
+        let node = top;
+        while (
+            (left(node) < this.size() && this._greater(left(node), node)) ||
+            (right(node) < this.size() && this._greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+            this._swap(node, maxChild);
+            node = maxChild;
+        }
+    }
+}
+
+var lastStoneWeight = function(stones) {
+    const q = new PriorityQueue();
+    for (let i of stones) q.push(i);
+    while (q.size() > 1) {
+        let t1 = q.pop();
+        let t2 = q.pop();
+        if (t1 != t2) q.push(t1 - t2);
+    }
+    return q.size() == 1 ? q.peek() : 0;
+};
