8b15a4654b8a177a638f1e28a5f6e42c3f32299e
coordinate <luliyucoordinate@outlook.com>
2019-09-30 18:16:23 +0800

    add 1199 js

---
+++ src/1199-Minimum-Time-to-Build-Blocks/1199.js
@@ -0,0 +1,88 @@
+const t = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+
+class PriorityQueue {
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+
+    size() {
+        return this._heap.length;
+    }
+
+    isEmpty() {
+        return this.size() == 0;
+    }
+
+    peek() {
+        return this._heap[t];
+    }
+
+    push(...values) {
+        values.forEach(value => {
+            this._heap.push(value);
+            this._siftUp();
+        });
+        return this.size();
+    }
+
+    pop() {
+        const poppedValue = this.peek();
+        const bottom = this.size() - 1;
+        if (bottom > t) {
+            this._swap(t, bottom);
+        }
+        this._heap.pop();
+        this._siftDown();
+        return poppedValue;
+    }
+
+    replace(value) {
+        const replacedValue = this.peek();
+        this._heap[t] = value;
+        this._siftDown();
+        return replacedValue;
+    }
+
+    _greater(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+
+    _siftUp() {
+        let node = this.size() - 1;
+        while (node > t && this._greater(node, parent(node))) {
+            this._swap(node, parent(node));
+            node = parent(node);
+        }
+    }
+
+    _siftDown() {
+        let node = t;
+        while (
+            (left(node) < this.size() && this._greater(left(node), node)) ||
+            (right(node) < this.size() && this._greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+            this._swap(node, maxChild);
+            node = maxChild;
+        }
+    }
+}
+
+var minBuildTime = function(blocks, split) {
+    const q = new PriorityQueue((a, b) => a < b);
+    for (let i of blocks) q.push(i);
+    while (q.size() > 1) {
+        q.pop();
+        let y = q.pop();
+        q.push(y + split);
+    }
+    return q.peek();
+};
