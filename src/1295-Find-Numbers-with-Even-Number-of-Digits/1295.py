05ac73d7f38f629785b8272d597ea8b3a1132bee
Your Name <luliyucoordinate@outlook.com>
2019-12-27 09:18:12 +0800

    more pythonic for 1295

--- src/1295-Find-Numbers-with-Even-Number-of-Digits/1295.py
+++ src/1295-Find-Numbers-with-Even-Number-of-Digits/1295.py
@@ -1,8 +1,3 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        res = 0
-        for n in nums:
-            x = int(math.log(n, 10) + 1)
-            if x % 2 == 0:
-                res += 1
-        return res
+        return sum(map(lambda n: len(str(n)) % 2 == 0, nums))  
