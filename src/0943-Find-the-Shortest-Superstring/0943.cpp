2a3504651d5baec0613dd92426468f5a3c82719b
luliyucoordinate <luliyucoordinate@outlook.com>
2020-02-06 15:53:31 +0800

    fic 943

--- src/0943-Find-the-Shortest-Superstring/0943.cpp
+++ src/0943-Find-the-Shortest-Superstring/0943.cpp
@@ -1,137 +1,75 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-using namespace std;
-
-static int x = []() {std::ios::sync_with_stdio(false); cin.tie(0); return 0; }();
 class Solution 
 {
-    const short validMask[12] = {0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80,0x100,0x200,0x400,0x800};
-        
-    class MyNode;
-    vector<MyNode> nodes;
-    
-    class MyNode {
-    public:
-        MyNode(short i, short N, string s):idx(i),min(0x3FFF),str(s){edges.resize(N,-1);}
-        short idx;
-        short min;
-        vector<short> edges;
-        string str;
-    };
-    
-    class MyState {
-    public:
-        MyState(short c, short d, short v, MyState* p):cur(c),dis(d),valid(v),prevState(p){}
-        short cur;
-        short dis;
-        short valid;
-        MyState* prevState;
-        void print() {cout << cur << " " << dis << " " << valid << endl;}
-    };
-    
-    struct MyStateCmp {
-        bool operator()(const MyState* a, const MyState* b) {
-            return a->dis > b->dis;
-        }
-    };
-    
+public:
     string shortestSuperstring(vector<string>& A) 
-    {        
-        int N = A.size();
-        short validFull = (1<<N)-1;
+    {
+        int n = A.size(), vis = (1 << n) - 1;
+        int dp[1 << n][n], path[1 << n][n], g[n][n];
+        memset(dp, 0x3f, sizeof dp);
+        memset(g, 0, sizeof g);
+        memset(path, 0, sizeof path);
         
-        nodes.clear();
-        for (int i = 0; i < N; ++i) nodes.push_back(MyNode(i,N,A[i]));
-        
-        for (int i = 0; i < N; ++i) 
+        for (int i = 0; i < n; i++)
         {
-            MyNode& curNode = nodes[i];
-            const string & lStr = A[i];
-            for (int j = 0; j < N; ++j) 
+            for (int j = 0; j < n; j++)
             {
                 if (i == j) continue;
-                MyNode& nextNode = nodes[j];
-                const string & rStr = A[j];
-                int maxOverlap = min(lStr.size(),rStr.size());
-                for (int k = maxOverlap; k >= 0; --k) 
-                {
-                    bool flag = true;
-                    for (int l = 0; l < k; ++l) 
-                    {
-                        if (lStr[lStr.size()-k+l] != rStr[l]) 
-                        {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) 
-                    {
-                        curNode.edges[j] = rStr.size()-k;
-                        if (curNode.edges[j] < nextNode.min) 
-                        {
-                            nextNode.min = curNode.edges[j];
-                        }
-                        break;
-                    }
-                }
+                g[i][j] = dis(A[i], A[j]);
             }
         }
-
-        for (int i = 0; i < N; ++i) 
-        {
-            MyNode& curNode = nodes[i];
-            for (int j = 0; j < N; ++j) 
-            {
-                if (i == j) continue;
-                MyNode& nextNode = nodes[j];
-                curNode.edges[j] -= nextNode.min;
-            }
-        }
-        priority_queue<MyState*,vector<MyState*>,MyStateCmp> pq;
         
-        for (int i = 0; i < N; ++i) 
+        int best = 300, pos = -1, pre, p;
+        for (int i = 0; i < (1 << n); i++)
         {
-            pq.push(new MyState(i,A[i].size()-nodes[i].min,validFull^validMask[i],NULL));
-        }
-        
-        int count = 0;
-        while (!pq.empty()) 
-        {
-            MyState* curState = pq.top(); pq.pop(); ++count;
-            if (curState->valid == 0) 
+            for (int j = 0; j < n; j++)
             {
-                stack<short> btS;
-                while(curState!=NULL) { btS.push(curState->cur); curState=curState->prevState; }
+                if (((i >> j) & 1) == 0) continue;
                 
-                string ret = A[btS.top()];
-                while (!btS.empty()) 
+                pre = i - (1 << j);
+                if (pre == 0) dp[i][j] = A[j].size();
+                else
                 {
-                    short curI = btS.top(); btS.pop(); 
-                    if (btS.empty()) break;
-                    short nextI = btS.top();  
-                    ret += A[nextI].substr(A[nextI].size()-nodes[curI].edges[nextI]-nodes[nextI].min);
-                }
-                return ret;
-            } 
-            else 
-            {
-                for (int i = 0; i < N; ++i) 
-                {
-                    if (curState->valid & validMask[i]) 
+                    for (int k = 0; k < n; k++)
                     {
-                        short nextDis = curState->dis + nodes[curState->cur].edges[i];
-                        pq.push(new MyState(i,nextDis,curState->valid^validMask[i],curState));
+                        if (((pre >> k) & 1) == 0) continue;
+                        
+                        p = dp[pre][k] + g[k][j];
+                        if (dp[i][j] > p)
+                        {
+                            dp[i][j] = p, path[i][j] = k;
+                        }
                     }
+                }
+                
+                if (i == vis && best > dp[i][j])
+                {
+                    best = dp[i][j], pos = j;
                 }
             }
         }
-        return "";
+        
+        string res = A[pos];
+        int t = vis;
+        while (true)
+        {
+            pre = path[t][pos];
+            t -= (1 << pos);
+            if (t == 0) break;
+            res = A[pre] + res.substr(A[pos].size() - g[pre][pos]);
+            pos = pre;
+        }
+        return res;
+    }
+    
+private:
+    int dis(string& a, string& b)
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a.size() - i <= b.size() && 
+                a.substr(i) == b.substr(0, a.size() - i))
+                return b.size() - a.size() + i;
+        }
+        return b.size();
     }
 };
-int main()
-{
-    string A = {"catg","ctaagt","gcta","ttca","atgcatc"};
-    cout << Solution().shortestSuperstring(A);
-    return 0;
-}
--- src/0943-Find-the-Shortest-Superstring/0943.py
+++ src/0943-Find-the-Shortest-Superstring/0943.py
@@ -1,50 +1,47 @@
 class Solution:
-    def shortestSuperstring(self, A):
-        """
-        :type A: List[str]
-        :rtype: str
-        """
-        def findOverlappingPair(s1, s2):
-            max_overlap_len = float("-inf") 
-            n = min(len(s1), len(s2))
-            res = ''
-            for i in range(1, n + 1):
-                if s1.endswith(s2[:i]):
-                    if max_overlap_len < i:
-                        max_overlap_len = i
-                        res = s1 + s2[i:]
-            for i in range(1, n + 1):
-                if s2.endswith(s1[:i]):
-                    if max_overlap_len < i:
-                        max_overlap_len = i
-                        res = s2 + s1[i:]
+    def shortestSuperstring(self, A: List[str]) -> str:
+        n = len(A)
+        g = [[0] * n for _ in range(n)]
+        path = [[0] * n for _ in range(1 << n)]
+        vis = (1 << n) - 1
+        
+        def dis(a, b):
+            for i in range(len(a)):
+                if len(a) - i <= len(b) and a[i:] == b[:len(a) - i]:
+                    return len(b) - len(a) + i
+            return len(b)
+        
+        for i in range(n):
+            for j in range(n):
+                if i == j: continue
+                g[i][j] = dis(A[i], A[j]) #计算字符串距离
+        
+        dp = [[float("inf")] * n for _ in range(1 << n)]
+        best, pos = float("inf"), -1 #记录最优解的位置
+        
+        # TSP
+        for i in range(1 << n):
+            for j in range(n):
+                if ((i >> j) & 1) == 0: continue
+                pre = i - (1 << j)
+                if pre == 0: # j作为第一个位置开始
+                    dp[i][j] = len(A[j]) 
+                else:
+                    for k in range(n):
+                        if ((pre >> k) & 1) == 0: continue
+                        p = dp[pre][k] + g[k][j] # 如果pre>k + k->j < pre->j，进行放缩
+                        if dp[i][j] > p:
+                            dp[i][j], path[i][j] = p, k # path记录j之前的位置k
 
-            return max_overlap_len, res
-        
-        A_len = len(A)
-        
-        while A_len != 1:
-            p, q = -1, -1
-            res = ''
-            max_len = float("-inf") 
-            for i in range(A_len):
-                for j in range(i+1, A_len):
-                    overlap_len, tmp_res = findOverlappingPair(A[i], A[j])
-                    if max_len < overlap_len:
-                        max_len = overlap_len
-                        res = tmp_res
-                        p = j
-                        q = i
-            A_len -= 1
-            if max_len == float("-inf"): 
-                A[0] = A[0] + A[A_len]
-            else:
-                A[p] = res
-                A[q] = A[A_len]
+                if i == vis and best > dp[i][j]: # 寻找最优解
+                    best, pos = dp[i][j], j
+      
+        res, t = A[pos], vis
+        while 1:
+            pre = path[t][pos] # 依次弹出pos的前一个位置
+            t -= (1 << pos)
+            if t == 0: break
+            res = A[pre] + res[len(A[pos])-g[pre][pos]:]
+            pos = pre
             
-        return A[0]
-            
-        
-if __name__ == "__main__":
-    A = ["catg","ctaagt","gcta","ttca","atgcatc"]
-    print(Solution().shortestSuperstring(A))
+        return res
