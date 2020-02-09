132412963353c05497dc23319818c4d1827ae1d5
wujionghao <1004691415qq@gmail.com>
2020-02-09 13:31:53 +0800

    560 python
    改成hash map 的api会快一点

--- src/0560-Subarray-Sum-Equals-K/0560.py
+++ src/0560-Subarray-Sum-Equals-K/0560.py
@@ -1,3 +1,4 @@
+- Method 1
 class Solution:
     def subarraySum(self, nums, k):
         """
@@ -17,7 +18,23 @@ class Solution:
                 sum_dict[cur_sum] = 1
                 
         return result
-
+    
+- Method 2
+class Solution:
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result, cur_sum = 0, 0
+        sum_dict = {0:1}
+        for num in nums:
+            cur_sum += num
+            result += sum_dict.get(cur_sum - k,0)
+            sum_dict[cur_sum] = sum_dict.get(cur_sum, 0)+1
+          
+        return result
 if __name__ == "__main__":
     nums = [1,1,1]
     k = 2
