f1d3b04f42898158a64164b896162424e5005d14
luliyucoordinate <luliyucoordinate@outlook.com>
2020-02-10 16:51:27 +0800

    优化560 python 代码

--- src/0560-Subarray-Sum-Equals-K/0560.py
+++ src/0560-Subarray-Sum-Equals-K/0560.py
@@ -1,22 +1,14 @@
 class Solution:
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        result, cur_sum = 0, 0
-        sum_dict = {0:1}
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res, cur = 0, 0
+        sum_dict = collections.defaultdict(int)
+        sum_dict[0] = 1
+        
         for num in nums:
-            cur_sum += num
-            if cur_sum - k in sum_dict:
-                result += sum_dict[cur_sum - k]
-            if cur_sum in sum_dict:
-                sum_dict[cur_sum] += 1
-            else:
-                sum_dict[cur_sum] = 1
-                
-        return result
+            cur += num
+            res += sum_dict[cur - k]
+            sum_dict[cur] += 1
+        return res
 
 if __name__ == "__main__":
     nums = [1,1,1]
