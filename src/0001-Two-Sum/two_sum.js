9e8ea21b483b18c57d8080d4c474f3c8c7f91d22
Madhusudan Pareek <85008599+MadMods96@users.noreply.github.com>
2021-12-13 15:21:28 +0530

    added js solution

---
+++ src/0001-Two-Sum/two_sum.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; ++i) {
+        for (let j = i + 1; j < nums.length; ++j ) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
