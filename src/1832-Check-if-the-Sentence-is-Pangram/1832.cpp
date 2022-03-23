5c93ac8f4b1c1e0c8ecddcff2d2c9b05bcc82a1b
Snehel <79275152+snehel4510@users.noreply.github.com>
2022-03-24 00:38:23 +0530

    Update 1832.cpp

--- src/1832-Check-if-the-Sentence-is-Pangram/1832.cpp
+++ src/1832-Check-if-the-Sentence-is-Pangram/1832.cpp
@@ -1,20 +1,17 @@
 class Solution {
-public:
+ public:
     bool checkIfPangram(string sentence) {
         map<char,int> m;
         char a = 'a';
-        for(int i=0;i<26;i++)
-        {
+        for (int i = 0; i < 26; i++) {
             m.insert({a,0});
             a++;
         }
-        for(auto i:sentence)
-        {
+        for (auto i : sentence) {
             m[i]++;
         }
-        for(auto i:m)
-        {
-            if(i.second==0)
+        for (auto i : m) {
+            if (i.second == 0)
                 return false;
         }
         return true;
