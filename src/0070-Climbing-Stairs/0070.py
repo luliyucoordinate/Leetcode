07d438ce6ece79e158efd0c909574171b58b5582
luliyucoordinate <luliyucoordinate@outlook.com>
2022-12-12 20:11:57 +0800

    update 70

--- src/0070-Climbing-Stairs/0070.py
+++ src/0070-Climbing-Stairs/0070.py
@@ -1,21 +1,6 @@
 class Solution:
-    def _climbStairs(self, n, climb):
-        if n == 0 or n == 1:
-            return 1
-
-        if n not in climb:
-            climb[n] = self._climbStairs(n - 1, climb) + self._climbStairs(n - 2, climb)
-
-        return climb[n]
-
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        climb = dict()
-        return self._climbStairs(n, climb)
-
-if __name__ == '__main__':
-    n = 3
-    print(Solution().climbStairs(n))
+    def climbStairs(self, n: int) -> int:
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
