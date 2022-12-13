6f9420837c22ad4d47a721c393d2e90ef9cfdfba
luliyucoordinate <luliyucoordinate@outlook.com>
2022-12-13 20:39:35 +0800

    update 1832

--- src/1832-Check-if-the-Sentence-is-Pangram/1832.cpp
+++ src/1832-Check-if-the-Sentence-is-Pangram/1832.cpp
@@ -1,19 +1,14 @@
 class Solution {
  public:
-    bool checkIfPangram(string sentence) {
-        map<char,int> m;
-        char a = 'a';
-        for (int i = 0; i < 26; i++) {
-            m.insert({a,0});
-            a++;
-        }
-        for (auto i : sentence) {
-            m[i]++;
-        }
-        for (auto i : m) {
-            if (i.second == 0)
-                return false;
-        }
-        return true;
+  bool checkIfPangram(string sentence) {
+    int cnt[26] = {};
+    for (auto c : sentence) {
+      cnt[c - 'a']++;
     }
+    bool res = true;
+    for (int i = 0; i < 26; i++) {
+      res &= (cnt[i] >= 1);
+    }
+    return res;
+  }
 };
