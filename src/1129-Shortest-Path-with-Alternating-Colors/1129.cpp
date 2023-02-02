45d6757fd2b1362aed2c75df7017d626eef98a98
luliyucoordinate <luliyucoordinate@outlook.com>
2023-02-02 21:37:34 +0800

    update 1129 cpp

--- src/1129-Shortest-Path-with-Alternating-Colors/1129.cpp
+++ src/1129-Shortest-Path-with-Alternating-Colors/1129.cpp
@@ -1,34 +1,34 @@
-class Solution 
-{
-public:
-    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) 
-    {
-        vector<map<int, vector<int>>> g(2, map<int, vector<int>>());
-        for (auto& d : red_edges) g[0][d[0]].push_back(d[1]);
-        for (auto& d : blue_edges) g[1][d[0]].push_back(d[1]);
-        vector<int> res(n, INT_MAX);
-        
-        queue<vector<int>> q;
-        q.push({0, 0, 0}); q.push({0, 1, 0});
-        unordered_set<int> vis;
-        vis.insert(0); vis.insert(1);
-        
-        while (!q.empty())
-        {
-            auto pre = q.front(); q.pop();
-            res[pre[0]] = min(res[pre[0]], pre[2]);
-            int nc = 1 - pre[1];
-            for (auto& nd : g[nc][pre[0]])
-            {
-                int key = nd * 401 + nc;
-                if (!vis.count(key))
-                {
-                    q.push({nd, nc, pre[2] + 1});
-                    vis.insert(key);
-                }
-            }
+class Solution {
+ public:
+  vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges,
+                                       vector<vector<int>>& blue_edges) {
+    vector<vector<vector<int>>> g(2, vector<vector<int>>(n));
+    for (auto& e : red_edges) g[0][e[0]].push_back(e[1]);
+    for (auto& e : blue_edges) g[1][e[0]].push_back(e[1]);
+    vector<vector<bool>> vis(2, vector<bool>(n));
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    q.push({1, 0});
+
+    vector<int> res(n, -1);
+    int d = 0;
+    while (!q.empty()) {
+      auto qs = q.size();
+      while (qs--) {
+        auto [x, y] = q.front();
+        q.pop();
+        vis[x][y] = true;
+        auto c = 1 - x;
+        for (auto& v : g[c][y]) {
+          if (vis[c][v]) continue;
+          q.push({c, v});
         }
-        for (int i = 0; i < n; ++i) if (res[i] == INT_MAX) res[i] = -1;
-        return res;
+        if (res[y] == -1) res[y] = d;
+      }
+      d++;
     }
+
+    return res;
+  }
 };
