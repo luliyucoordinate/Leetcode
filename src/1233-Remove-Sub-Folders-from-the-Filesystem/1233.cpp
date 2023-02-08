ec779c2f6da910cf464d00bb0642dd3cb36de175
luliyucoordinate <luliyucoordinate@outlook.com>
2023-02-08 21:15:47 +0800

    update 1233

--- src/1233-Remove-Sub-Folders-from-the-Filesystem/1233.cpp
+++ src/1233-Remove-Sub-Folders-from-the-Filesystem/1233.cpp
@@ -1,21 +1,17 @@
-class Solution 
-{
-public:
-    vector<string> removeSubfolders(vector<string>& folder) 
-    {
-        sort(folder.begin(), folder.end());
-        vector<string> res;
-        int l = 2;
-        string parent = "";
-        for (auto& f : folder)
-        {
-            if (f.substr(0, l) != parent)
-            {
-                l = f.size() + 1;
-                parent = f + "/";
-                res.push_back(f);
-            }
-        }
-        return res;
+class Solution {
+ public:
+  vector<string> removeSubfolders(vector<string>& folder) {
+    sort(folder.begin(), folder.end());
+    vector<string> res;
+    int n = folder.size();
+    for (int i = 0, j = 1; i < n; j++) {
+      res.emplace_back(folder[i]);
+      auto a = folder[i].size();
+      while (j < n && a < folder[j].size() && folder[j][a] == '/' &&
+             folder[j].substr(0, a) == folder[i])
+        j++;
+      i = j;
     }
+    return res;
+  }
 };
