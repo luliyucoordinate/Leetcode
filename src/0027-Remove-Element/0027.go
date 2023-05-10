d7e621f9342a31e7f81673ae17ec521c026e6af0
cosmicray001 <samiulislambracu@gmail.com>
2023-05-10 23:31:29 +0600

    add: remove-element solution in Go

---
+++ src/0027-Remove-Element/0027.go
@@ -0,0 +1,12 @@
+package removeelement
+
+func removeElement(nums []int, val int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
